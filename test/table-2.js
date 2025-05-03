ffa26d7e26fd232640b9c4c731b9a2e18384b8f8
Seanghay Yath <seanghay.dev@gmail.com>
2025-05-03 20:29:25 +0700

    split into sub components

--- test/table-2.js
+++ test/table-2.js
@@ -12,191 +12,207 @@ import {
   renderAsPdfBuffer,
 } from "../src/sone.js";
 
-async function Document() {
-  const colors = {
-    primary: "#3A0519",
-    white: "#fff",
-    green: "#00AF50",
-    red: "#FF0101",
-    orange: "#E9A319",
-    black: "#333",
-    gray: "rgba(0,0,0,.2)",
-  };
+const colors = {
+  primary: "#222831",
+  white: "#fff",
+  green: "#00AF50",
+  red: "#FF0101",
+  orange: "#E9A319",
+  black: "#333",
+  gray: "rgba(0,0,0,.2)",
+};
 
-  const svgSrc = loadSvg(await fs.readFile("test/sone-white.svg", "utf8"));
+const svgSrc = loadSvg(await fs.readFile("test/sone.svg", "utf8"));
 
-  return Column(
+function StatusIndicator(text, color) {
+  return Row(
+    Row(Text(text).size(20).weight(500)).grow(1),
+    Row().size(60, 18).bg(color),
+  );
+}
+
+function Header(project) {
+  const KeyItem = (label) => Text(label).size(22).color(colors.white);
+  const ValueItem = (label) => Text(label).size(22).weight("bold");
+
+  return Row(
     Row(
       Row(
-        Row(
-          Row(Svg(svgSrc).size(90).scaleType("contain"))
-            .alignSelf("center")
-            .padding(38, 34),
-          Column(
-            Text("Project Name").size(22).color(colors.white),
-            Text("Project Manager").size(22).color(colors.white),
-            Text("Status Date").size(22).color(colors.white),
-          )
-            .gap(18)
-            .padding(10)
-            .alignSelf("center"),
-        ).bg(colors.primary),
+        Row(Svg(svgSrc).size(90).scaleType("contain"))
+          .alignSelf("center")
+          .padding(38, 34),
         Column(
-          Text("Project Title").size(22).weight("bold"),
-          Text("Seanghay").size(22).weight("bold"),
-          Text("01/05/2024 - 12:00 AM").size(22).weight("bold"),
+          KeyItem("Project Name"),
+          KeyItem("Project Manager"),
+          KeyItem("Status Date"),
         )
           .gap(18)
           .padding(10)
           .alignSelf("center"),
-      )
-        .gap(10)
-        .grow(1),
+      ).bg(colors.primary),
       Column(
-        Row(Text("Overall Status").size(22).weight("bold"))
-          .padding(15, 18)
-          .grow(1)
-          .alignItems("center"),
-        Row(
-          Column(
-            Row(
-              Row(Text("On Target").size(20).weight(500)).grow(1),
-              Row().size(60, 18).bg(colors.green),
-            ),
-            Row(
-              Row(Text("Risk").size(20).weight(500)).grow(1),
-              Row().size(60, 18).bg(colors.orange),
-            ),
-            Row(
-              Row(Text("Delay").size(20).weight(500)).grow(1),
-              Row().size(60, 18).bg(colors.red),
-            ),
-          )
-            .padding(10, 18)
-            .strokeColor(colors.primary)
-            .strokeWidth(4)
-            .gap(8)
-            .grow(1),
-          Row(
-            Row()
-              .bg(colors.green)
-              .size(56)
-              .cornerRadius(56)
-              .alignSelf("center"),
-          )
-            .padding(10)
-            .justifyContent("center")
-            .aspectRatio(1)
-            .strokeColor(colors.primary)
-            .strokeWidth(4),
-        ),
+        ValueItem(project.name),
+        ValueItem(project.manager),
+        ValueItem(project.date),
       )
-        .minWidth(350)
-        .strokeColor(colors.primary)
-        .strokeWidth(4),
+        .gap(18)
+        .padding(10)
+        .alignSelf("center"),
     )
-      .strokeWidth(4)
-      .strokeColor(colors.primary)
-      .margin(44, 44, 0, 44),
-
-    Row(
-      Column(
-        // section 1
+      .gap(10)
+      .grow(1),
+    Column(
+      Row(Text("Overall Status").size(22).weight("bold"))
+        .padding(15, 18)
+        .grow(1)
+        .alignItems("center"),
+      Row(
         Column(
-          Text("Achievement for This Week")
-            .size(22)
-            .color(colors.white),
+          StatusIndicator("On Target", colors.green),
+          StatusIndicator("Risk", colors.orange),
+          StatusIndicator("Delay", colors.red),
         )
-          .bg(colors.primary)
-          .padding(24, 14)
-          .strokeColor(colors.primary)
-          .strokeWidth(4),
-        Column(
-          Text(
-            "- Had an internal meeting with Donald Trump\n- Meeting and meeting",
-          )
-            .lineHeight(1.4)
-            .size(18)
-            .color(colors.black),
-        )
-          .padding(24)
+          .padding(10, 18)
           .strokeColor(colors.primary)
           .strokeWidth(4)
+          .gap(8)
           .grow(1),
-        // section 2
-        Column(Text("Plan for Next Week").size(22).color(colors.white))
-          .bg(colors.primary)
-          .padding(24, 14)
-          .strokeColor(colors.primary)
-          .strokeWidth(4),
-        Column(
-          Text("- Rocket Launch\n- Build something useful")
-            .lineHeight(1.4)
-            .size(18)
-            .color(colors.black),
+        Row(
+          Row()
+            .bg(project.status)
+            .size(56)
+            .cornerRadius(56)
+            .alignSelf("center"),
         )
-          .padding(24)
-          .strokeColor(colors.primary)
-          .strokeWidth(4)
-          .grow(1),
-        // section 3
-        Column(
-          Table(
-            TableRow(
-              Row(Text("Issue or Risk").color(colors.white).size(20))
-                .bg(colors.primary)
-                .padding(8, 10),
-              Row(Text("Suggest Solution").color(colors.white).size(20))
-                .bg(colors.primary)
-                .padding(8, 10),
-            ),
-            TableRow(
-              Row(Text("Issue 1").size(20).color(colors.black)).padding(8, 10),
-              Row(Text("Solution 1").size(20).color(colors.black)).padding(
-                8,
-                10,
-              ),
-            ),
-            TableRow(
-              Row(Text("Issue 2").size(20).color(colors.black)).padding(8, 10),
-              Row(Text("Solution 2").size(20).color(colors.black)).padding(
-                8,
-                10,
-              ),
-            ),
-            TableRow(
-              Row(Text("Issue 3").size(20).color(colors.black)).padding(8, 10),
-              Row(Text("Solution 3").size(20).color(colors.black)).padding(
-                8,
-                10,
-              ),
-            ),
-          )
-            .strokeColor(colors.gray)
-            .strokeWidth(2),
-        )
+          .padding(10)
+          .justifyContent("center")
+          .aspectRatio(1)
           .strokeColor(colors.primary)
           .strokeWidth(4),
       ),
-      // Project timeline
+    )
+      .minWidth(350)
+      .strokeColor(colors.primary)
+      .strokeWidth(4),
+  )
+    .strokeWidth(4)
+    .strokeColor(colors.primary)
+    .margin(44, 44, 0, 44);
+}
+
+function Achievement(info) {
+  return [
+    Column(Text("Achievement for This Week").size(22).color(colors.white))
+      .bg(colors.primary)
+      .padding(24, 14)
+      .strokeColor(colors.primary)
+      .strokeWidth(4),
+    Column(Text(info).lineHeight(1.4).size(18).color(colors.black))
+      .padding(24)
+      .strokeColor(colors.primary)
+      .strokeWidth(4)
+      .grow(1),
+  ];
+}
+
+function NextPlan(info) {
+  return [
+    Column(Text("Plan for Next Week").size(22).color(colors.white))
+      .bg(colors.primary)
+      .padding(24, 14)
+      .strokeColor(colors.primary)
+      .strokeWidth(4),
+    Column(Text(info).lineHeight(1.4).size(18).color(colors.black))
+      .padding(24)
+      .strokeColor(colors.primary)
+      .strokeWidth(4)
+      .grow(1),
+  ];
+}
+
+function IssueItem(issue, solution) {
+  return TableRow(
+    Row(Text(issue).size(20).color(colors.black)).padding(8, 10),
+    Row(Text(solution).size(20).color(colors.black)).padding(8, 10),
+  );
+}
+
+function IssueOrRisk(items) {
+  const IssueHeaderCell = (label) =>
+    Row(Text(label).color(colors.white).size(20))
+      .bg(colors.primary)
+      .padding(8, 10);
+
+  return Column(
+    Table(
+      TableRow(
+        IssueHeaderCell(`Issue or Risk (${items.length})`),
+        IssueHeaderCell("Suggest Solution"),
+      ),
+      ...items.map((item) => IssueItem(item.issue, item.solution)),
+    )
+      .strokeColor(colors.gray)
+      .strokeWidth(2),
+  )
+    .strokeColor(colors.primary)
+    .strokeWidth(4);
+}
+
+function KeyMilestone(items) {
+  const HeaderCell = (label) =>
+    Row(Text(label).weight("bold").color(colors.white).size(18))
+      .bg(colors.primary)
+      .padding(8, 10);
+
+  const RowItem = (info, date, percent, color) =>
+    TableRow(
+      Row(Text(info).size(18).color("#333")).padding(4, 8),
+      Row(Text(date).size(18).color("#333")).padding(4, 8),
+      Row(Text(percent).size(18).color("#333"))
+        .justifyContent("center")
+        .padding(4, 8),
+      Row(Row().size(18).cornerRadius(18).bg(color).alignSelf("center"))
+        .padding(4, 8)
+        .justifyContent("center"),
+    );
+
+  return Table(
+    TableRow(
+      HeaderCell("Key Milestones"),
+      HeaderCell("Due date"),
+      HeaderCell("Complete"),
+      HeaderCell("Status"),
+    ),
+    ...items.map((values) => RowItem(...values)),
+  )
+    .strokeWidth(3)
+    .strokeColor("rgba(0,0,0,.1)");
+}
+
+function Document(data) {
+  const ProjectTimelineHeaderCell = (label) =>
+    Row(Text(label).size(18).weight(500)).padding(6, 12).bg(colors.gray);
+
+  const TrackBar = (start, end, color) =>
+    Row().size(end, 14).left(start).bg(color).cornerRadius(14);
+
+  return Column(
+    Header(data.project),
+    Row(
+      Column(
+        ...Achievement(data.achivements.join("\n")),
+        ...NextPlan(data.plans.join("\n")),
+        IssueOrRisk(data.risks),
+      ),
       Column(
         Column(Text("Project Timeline - 2025").size(22).color(colors.white))
           .bg(colors.primary)
           .padding(8, 20),
         Table(
           TableRow(
-            Row(Text("Mar").size(18).weight(500))
-              .padding(6, 12)
-              .bg(colors.gray),
-            Row(Text("Apr").size(18).weight(500))
-              .padding(6, 12)
-              .bg(colors.gray),
-            Row(Text("May").size(18).weight(500))
-              .padding(6, 12)
-              .bg(colors.gray),
-            Row(Text("June").size(18).weight(500))
-              .padding(6, 12)
-              .bg(colors.gray),
+            ...["Mar", "Apr", "May", "June"].map((value) =>
+              ProjectTimelineHeaderCell(value),
+            ),
           ),
         )
           .marginTop(10)
@@ -204,28 +220,15 @@ async function Document() {
           .strokeWidth(4),
         Row(
           Column(
-            Row().size(300, 14).bg(colors.gray).cornerRadius(0),
-            Row()
-              .size(340, 14)
-              .bg(colors.orange)
-              .cornerRadius(0)
-              .marginLeft(50),
-            Row()
-              .size(400, 14)
-              .marginLeft(100)
-              .bg(colors.green)
-              .cornerRadius(0),
-            Row().size(300, 14).marginLeft(200).bg(colors.red).cornerRadius(0),
-            Row()
-              .size(320, 14)
-              .marginLeft(300)
-              .bg(colors.primary)
-              .cornerRadius(0),
+            ...data.tracks.map(({ start, end, color }) =>
+              TrackBar(`${start}%`, `${end - start}%`, color),
+            ),
           )
+            .grow(1)
             .marginTop(8)
             .gap(8),
           Column(
-            Row().size(3, 150).bg("rgba(255,0,0,.6)"),
+            Row().size(3, 150).bg("rgba(255,0,0,.6)").cornerRadius(4),
             Text("We are here ", Span("🔥").offsetY(-4))
               .size(18)
               .color(colors.red),
@@ -234,115 +237,11 @@ async function Document() {
             .gap(8)
             .position("absolute")
             .top(10)
-            .left(440),
+            .width(150)
+            .left(-150 / 2)
+            .marginLeft(`${data.progress}%`),
         ).height(200),
-        Table(
-          TableRow(
-            Row(
-              Text("Key Milestones")
-                .weight("bold")
-                .color(colors.white)
-                .size(18),
-            )
-              .bg(colors.primary)
-              .padding(8, 10),
-            Row(Text("Due date").weight("bold").color(colors.white).size(18))
-              .bg(colors.primary)
-              .padding(8, 10),
-            Row(Text("Complete").weight("bold").color(colors.white).size(18))
-              .bg(colors.primary)
-              .padding(8, 10),
-            Row(Text("Status").weight("bold").color(colors.white).size(18))
-              .bg(colors.primary)
-              .padding(8, 10),
-          ),
-          TableRow(
-            Row(Text("Project Study").size(18).color("#333")).padding(4, 8),
-          ),
-          TableRow(
-            Row(Text("- Initial Meeting").size(18).color("#333")).padding(4, 8),
-            Row(Text("20/01/2024").size(18).color("#333")).padding(4, 8),
-            Row(Text("100%").size(18).color("#333"))
-              .justifyContent("center")
-              .padding(4, 8),
-            Row(
-              Row()
-                .size(18)
-                .cornerRadius(18)
-                .bg(colors.green)
-                .alignSelf("center"),
-            )
-              .padding(4, 8)
-              .justifyContent("center"),
-          ),
-          TableRow(
-            Row(Text("- Initial Meeting").size(18).color("#333")).padding(4, 8),
-            Row(Text("20/01/2024").size(18).color("#333")).padding(4, 8),
-            Row(Text("100%").size(18).color("#333"))
-              .justifyContent("center")
-              .padding(4, 8),
-            Row(
-              Row()
-                .size(18)
-                .cornerRadius(18)
-                .bg(colors.green)
-                .alignSelf("center"),
-            )
-              .padding(4, 8)
-              .justifyContent("center"),
-          ),
-          TableRow(
-            Row(Text("- Initial Meeting").size(18).color("#333")).padding(4, 8),
-            Row(Text("20/01/2024").size(18).color("#333")).padding(4, 8),
-            Row(Text("100%").size(18).color("#333"))
-              .justifyContent("center")
-              .padding(4, 8),
-            Row(
-              Row()
-                .size(18)
-                .cornerRadius(18)
-                .bg(colors.orange)
-                .alignSelf("center"),
-            )
-              .padding(4, 8)
-              .justifyContent("center"),
-          ),
-          TableRow(
-            Row(Text("- Initial Meeting").size(18).color("#333")).padding(4, 8),
-            Row(Text("20/01/2024").size(18).color("#333")).padding(4, 8),
-            Row(Text("100%").size(18).color("#333"))
-              .justifyContent("center")
-              .padding(4, 8),
-            Row(
-              Row()
-                .size(18)
-                .cornerRadius(18)
-                .bg(colors.red)
-                .alignSelf("center"),
-            )
-              .padding(4, 8)
-              .justifyContent("center"),
-          ),
-
-          TableRow(
-            Row(Text("- Initial Meeting").size(18).color("#333")).padding(4, 8),
-            Row(Text("20/01/2024").size(18).color("#333")).padding(4, 8),
-            Row(Text("100%").size(18).color("#333"))
-              .justifyContent("center")
-              .padding(4, 8),
-            Row(
-              Row()
-                .size(18)
-                .cornerRadius(18)
-                .bg(colors.red)
-                .alignSelf("center"),
-            )
-              .padding(4, 8)
-              .justifyContent("center"),
-          ),
-        )
-          .strokeWidth(3)
-          .strokeColor("rgba(0,0,0,.1)"),
+        KeyMilestone(data.milestones),
       ).grow(1),
     )
       .margin(14, 44, 44, 44)
@@ -354,5 +253,38 @@ async function Document() {
     .bg("#fff");
 }
 
-await fs.writeFile("test/table-2.jpg", renderAsImageBuffer(await Document()));
-await fs.writeFile("test/table-2.pdf", renderAsPdfBuffer(await Document()));
+const data = {
+  project: {
+    manager: "Seanghay",
+    name: "Project Name",
+    date: "01/05/2025 - 12:00 AM",
+    status: colors.green,
+  },
+  achivements: [
+    "- Had an internal meeting with Donald Trump",
+    "- Meeting and meeting",
+  ],
+  plans: ["- Rocket Launch", "- Build something useful"],
+  risks: [
+    { issue: "Issue 1", solution: "Solution 1" },
+    { issue: "Issue 2", solution: "Solution 2" },
+    { issue: "Issue 3", solution: "Solution 3" },
+  ],
+  milestones: [
+    ["Project Study", "", "", ""],
+    ["Initial Meeting 0", "20/01/2025", "100%", colors.green],
+    ["Initial Meeting 1", "20/01/2025", "100%", colors.red],
+    ["Initial Meeting 2", "20/01/2025", "100%", colors.orange],
+  ],
+  tracks: [
+    { start: 0, end: 20, color: colors.gray },
+    { start: 20, end: 40, color: colors.gray },
+    { start: 30, end: 60, color: colors.gray },
+    { start: 50, end: 80, color: colors.gray },
+    { start: 70, end: 100, color: colors.green },
+  ],
+  progress: 80,
+};
+
+await fs.writeFile("test/table-2.jpg", renderAsImageBuffer(Document(data)));
+await fs.writeFile("test/table-2.pdf", renderAsPdfBuffer(Document(data)));
