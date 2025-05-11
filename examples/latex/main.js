7e18089723e85a40c013ef1ff2a4bee3d874ed32
Seanghay Yath <seanghay.dev@gmail.com>
2025-05-11 22:30:02 +0700

    use usvgjs

--- examples/latex/main.js
+++ examples/latex/main.js
@@ -1,21 +1,17 @@
-import { Resvg } from "@resvg/resvg-js";
 import fs from "node:fs/promises";
 import { Column, Photo, Text, loadImage, sone } from "sonejs";
 import texsvg from "texsvg";
+import initUSvg, { optimize } from "usvgjs";
 
-const svg = await texsvg("x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}", {
-  optimize: true,
+const wasmBuffer = await fs.readFile("node_modules/usvgjs/usvgjs_bg.wasm");
+await initUSvg({
+  module_or_path: wasmBuffer,
 });
 
-const resvg = new Resvg(svg, {
-  fitTo: {
-    mode: "width",
-    value: 256,
-  },
-});
+let svg = await texsvg("x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}");
+svg = optimize(svg);
 
-const pngBuffer = resvg.render().asPng();
-const image = await loadImage(pngBuffer);
+const image = await loadImage(Buffer.from(svg));
 
 const Document = () =>
   Column(
@@ -27,3 +23,4 @@ const Document = () =>
     .gap(10);
 
 await fs.writeFile("output.png", await sone(Document).png());
+await fs.writeFile("output.pdf", await sone(Document).pdf());
