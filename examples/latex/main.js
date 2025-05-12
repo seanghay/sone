2888ea16e3625223cf6eb758261a5401d3cde27c
Seanghay Yath <seanghay.dev@gmail.com>
2025-05-12 08:36:55 +0700

    latex example

--- examples/latex/main.js
+++ examples/latex/main.js
@@ -1,9 +1,14 @@
 import fs from "node:fs/promises";
 import { Column, Photo, Text, loadImage, sone } from "sonejs";
 import texsvg from "texsvg";
-import initUSvg, { optimize } from "usvgjs";
 
-const wasmBuffer = await fs.readFile("node_modules/usvgjs/usvgjs_bg.wasm");
+import initUSvg, { optimize } from "usvgjs";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const wasmFile = require.resolve("usvgjs/usvgjs_bg.wasm");
+const wasmBuffer = await fs.readFile(wasmFile);
+
 await initUSvg({
   module_or_path: wasmBuffer,
 });
@@ -15,12 +20,11 @@ const image = await loadImage(Buffer.from(svg));
 
 const Document = () =>
   Column(
-    Text("Hello world").size(64).color("red"),
+    Text("Hello world").size(50).color("red"),
     Photo(image).scaleType("fit"),
   )
-    .padding(18)
-    .bg("#eee")
-    .gap(10);
+    .padding(40)
+    .gap(20);
 
 await fs.writeFile("output.png", await sone(Document).png());
 await fs.writeFile("output.pdf", await sone(Document).pdf());
