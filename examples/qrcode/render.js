26a7f92797a26e3d709c95a399bcb70ae0652acd
Seanghay Yath <seanghay.dev@gmail.com>
2025-05-13 15:00:34 +0700

    chore: add qrcode example

--- examples/qrcode/render.js
+++ examples/qrcode/render.js
@@ -1,33 +1,31 @@
 import fs from "node:fs/promises";
-import { sone, Column, Text, Photo, loadImage } from "sone";
-import QRCode from "qrcode-svg";
-
-const qrcode = new QRCode({
-  content: "This is awesome!",
-  padding: 0,
-  background: "slateblue",
-  color: "white",
-  join: true,
-});
-
-const svg = await loadImage(Buffer.from(qrcode.svg()));
+import { sone, Column, Text, Photo, qrcode } from "sone";
 
 function Widget() {
   return Column(
     Column(
-      Photo(svg),
-      Text("Hello world").size(24).align("center").color("white"),
+      Photo(
+        qrcode("https://github.com/seanghay/sone", {
+          margin: 0,
+          background: null,
+          color: "white",
+          ecl: "H",
+          size: 350,
+        }),
+      ),
+      Text("npm install sone")
+        .size(32)
+        .align("center")
+        .color("white")
+        .font("Geist Mono"),
     )
-      .padding(24)
-      .bg("slateblue")
-      .gap(20)
+      .padding(44)
+      .bg("linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%)")
+      .gap(32)
       .cornerRadius(24)
       .cornerSmoothing(0.7),
   ).padding(10);
 }
 
-await fs.writeFile(
-  "out.png",
-  await sone(Widget, { backgroundColor: null }).png(),
-);
+await fs.writeFile("out.png", await sone(Widget).png());
 await fs.writeFile("out.pdf", await sone(Widget).pdf());
