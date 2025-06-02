a33ba82b58948b51715c7f5bc9126dc270ff7c56
Seanghay Yath <seanghay.dev@gmail.com>
2025-06-02 12:00:37 +0700

    fix: file name

--- examples/viewer/src/main.jsx
+++ examples/viewer/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
-import { Studio } from "./Studio";
+import { Studio } from "./studio";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
